c7ecb90329186edc85e0042873c6ad91279fd90f
HamzehBanymelhe <>
2021-06-02 21:00:43 +0000

    add index.html

---
+++ app.js
@@ -0,0 +1,10 @@
+var title="GOAL";
+var subtitle="   football news "
+var x=title+subtitle
+console.log(x)
+alert('goal web')
+var y = prompt('are you like football if yes write yes in blank else write no ')
+
+if( y > yes){
+  alert('you are good to cont')}
+else{ alert('you are not allowed to enter the website')}
