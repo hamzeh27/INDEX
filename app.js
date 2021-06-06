7ffed2924b68906947556faff39ed1f9959875e9
HamzehBanymelhe <>
2021-06-06 15:18:45 +0000

    add style.css

--- app.js
+++ app.js
@@ -9,14 +9,12 @@ while(p !=='2022')
 {p= prompt("the anwser is wrong enter the right year  ")}
 var z = prompt('What is your favorite team')
 document.write( z)
-
 var e = prompt('how many times real madrid won la liga ')
 while(e!=='34')
 {e = prompt('how many times real madrid won la liga')}
 var x=5;
 for(var i=0;i<x;i++)
- {
-   document.write('<img src ="https://phantom-marca.unidadeditorial.es/2cb42199cecee030b9062cdf9fc20a73/resize/1320/f/jpg/assets/multimedia/imagenes/2021/05/25/16219502015476.jpg" height="100px" width="100px"/>')
-   console.log(i)
-
- }
+{
+  document.write('<img src ="https://phantom-marca.unidadeditorial.es/2cb42199cecee030b9062cdf9fc20a73/resize/1320/f/jpg/assets/multimedia/imagenes/2021/05/25/16219502015476.jpg" height="100px" width="100px"/>')
+  console.log(i)
+}
