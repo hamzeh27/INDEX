37a27917e34554dfdf2b7b8a0cfce6749d1d8546
HamzehBanymelhe <>
2021-06-06 00:52:19 +0000

    readme.md

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var title="GOAL";
 var subtitle="   football news "
-var x=title+subtitle
+var x=title+subtitle 
 console.log(x)
 alert('goal web')
-var y = prompt('are you like football if yes write yes in blank else write no ')
+var p = prompt('are you like football if yes write yes in blank else write no ')
+var p = prompt ('in what year will qatar host the world cup?')
+while(p !=='2022')
+{p= prompt("the anwser is wrong enter the right year  ")}
+var z = prompt('What is your favorite team')
+document.write( z)
 
-if( y > yes){
-  alert('you are good to cont')}
-else{ alert('you are not allowed to enter the website')}
+var e = prompt('how many times real madrid won la liga ')
+while(e!=='34')
+{e = prompt('how many times real madrid won la liga')}
+var x=5;
+for(var i=0;i<x;i++)
+ {
+   document.write('<img src ="https://phantom-marca.unidadeditorial.es/2cb42199cecee030b9062cdf9fc20a73/resize/1320/f/jpg/assets/multimedia/imagenes/2021/05/25/16219502015476.jpg" height="100px" width="100px"/>')
+   console.log(i)
+
+ }
